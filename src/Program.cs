4d872bbb0480246435c0c2bd14f45a20704cd89b
shahad alhenaki <shahadkh00@gmail.com>
2024-04-15 16:57:05 +0300

    format

--- src/Program.cs
+++ src/Program.cs
@@ -3,50 +3,47 @@
 internal class Program
 {
 
-public class Base {
+    public class Base
+    {
+    }
 
-}
+    public class Book
+    {
+        int Id;
+        string? Title;
+        DateTime CreatedDate;
 
-public class Book {
-    int Id;
-    string? Title;
-    DateTime CreatedDate;
-
- public Book (int id, string title ){
-    Id = id;
-    Title = title;
-    CreatedDate = DateTime.Now;
- }
- } 
-
-
-
-
-public class User {
-    int Id;
-    string? Name;
-     DateTime CreatedDate;
-
-public User(int id, string name){
-    Id = id;
-    Name = name;
-    CreatedDate = DateTime.Now; 
-
-}}
-
-
-public class Library {}
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+            CreatedDate = DateTime.Now;
+        }
+    }
 
 
 
+
+    public class User
+    {
+        int Id;
+        string? Name;
+        DateTime CreatedDate;
+
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            CreatedDate = DateTime.Now;
+
+        }
+    }
+
+
+    public class Library { }
 
 
     private static void Main()
     {
-
-
-
-
-
     }
 }
