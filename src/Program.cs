84a24d7292181c1cddadbbbcef7607016349ac97
shahad alhenaki <shahadkh00@gmail.com>
2024-04-15 16:49:13 +0300

    Create classes

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,52 @@
-﻿internal class Program
+﻿using System.Data.Common;
+
+internal class Program
 {
+
+public class Base {
+
+}
+
+public class Book {
+    int Id;
+    string? Title;
+    DateTime CreatedDate;
+
+ public Book (int id, string title ){
+    Id = id;
+    Title = title;
+    CreatedDate = DateTime.Now;
+ }
+ } 
+
+
+
+
+public class User {
+    int Id;
+    string? Name;
+     DateTime CreatedDate;
+
+public User(int id, string name){
+    Id = id;
+    Name = name;
+    CreatedDate = DateTime.Now; 
+
+}}
+
+
+public class Library {}
+
+
+
+
+
     private static void Main()
     {
+
+
+
+
+
     }
 }
